7c844bb4cbfde697b9300441a2daa4eba6473d9a
Gianni <gianniabsillis@gmail.com>
2015-02-24 21:53:11 -0500

    finished UI`

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,6 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
@@ -10,7 +12,10 @@ import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSlider;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JToggleButton;
 import javax.swing.WindowConstants;
@@ -29,6 +34,11 @@ public class GUI {
 
 	public Team team;
 
+	//Panel for main GUI
+	public JPanel GUIPanel = new JPanel();
+	// Panel for comment box because gridbag is just annoying
+	public JPanel commentPanel = new JPanel();
+	
 	// Labels
 	public JLabel teamNumLabel = new JLabel("Team Number");
 	public JLabel matchNumLabel = new JLabel("Match Number");
@@ -42,23 +52,28 @@ public class GUI {
 	public JLabel humanPlayerLabel = new JLabel("Human Player");
 	public JLabel autoTypeLabel = new JLabel("Autonomous Type");
 	public JLabel reliabilityLabel = new JLabel("Reliablility");
+	public JLabel commentsLabel = new JLabel("Comments");
 
 	// Inputs
 	public JTextField teamNumberInputField = new JTextField(4);
 	public JTextField matchNumberField = new JTextField(4);
-	public JComboBox<String> alliancePicker = new JComboBox<String>(
-			ALLIANCE_COLORS);
+	// TextArea start
+	public JTextArea comments = new JTextArea(10,15);
+	public JScrollPane commentContainer = new JScrollPane(comments);
+	//TextArea End
+	public JComboBox<String> alliancePicker = new JComboBox<String>(ALLIANCE_COLORS);
 	public JCheckBox binBox = new JCheckBox();
 	public JCheckBox visionBox = new JCheckBox();
 	public JCheckBox litterBox = new JCheckBox();
 	public JTextField indivPointsInput = new JTextField(15);
 	public JTextField totalPointsField = new JTextField(15);
 	public JButton incrementStackButton = new JButton("0");
-	public JComboBox<String> humanPlayerPicker = new JComboBox<String>(
-			HUMAN_PLAYER_LEVELS);
+	public JComboBox<String> humanPlayerPicker = new JComboBox<String>(HUMAN_PLAYER_LEVELS);
 	public JComboBox<String> autoTypePicker = new JComboBox<String>(AUTO_TYPES);
 	public JToggleButton malfunctionButton = new JToggleButton("MALFUNCTION");
 	public JButton stackTimer = new JButton("STACKED A TOTE/BIN!");
+	public JButton matchStart = new JButton("Start Match!");
+	public JButton matchEnd = new JButton("Match over/ new match");
 	public JSlider reliabilitySlider = new JSlider(MIN_RELIABILITY, MAX_RELIABILITY);
 
 	public GUI() {
@@ -73,61 +88,61 @@ public class GUI {
 		window.setIconImage(new ImageIcon("robodog.png").getImage());
 		GridBagLayout layout = new GridBagLayout();
 		GridBagConstraints gbc = new GridBagConstraints();
-		window.setLayout(layout);
+		GUIPanel.setLayout(layout);
 
 		gbc.gridx = 0;
 		gbc.gridy = 0;
 		gbc.anchor = GridBagConstraints.EAST;
-		window.add(teamNumLabel, gbc);
+		GUIPanel.add(teamNumLabel, gbc);
 		gbc.gridx = 1;
 		gbc.anchor = GridBagConstraints.WEST;
-		window.add(teamNumberInputField, gbc);
+		GUIPanel.add(teamNumberInputField, gbc);
 		gbc.gridx = 0;
 		gbc.gridy = 1;
 		gbc.anchor = GridBagConstraints.EAST;
-		window.add(matchNumLabel, gbc);
+		GUIPanel.add(matchNumLabel, gbc);
 		gbc.gridx = 1;
 		gbc.anchor = GridBagConstraints.WEST;
-		window.add(matchNumberField, gbc);
+		GUIPanel.add(matchNumberField, gbc);
 		gbc.gridy = 2;
 		gbc.gridx = 0;
 		gbc.anchor = GridBagConstraints.EAST;
-		window.add(allianceLabel, gbc);
+		GUIPanel.add(allianceLabel, gbc);
 		gbc.gridx = 1;
 		gbc.anchor = GridBagConstraints.WEST;
-		window.add(alliancePicker, gbc);
+		GUIPanel.add(alliancePicker, gbc);
 		gbc.gridy = 3;
 		gbc.gridx = 1;
 		gbc.anchor = GridBagConstraints.WEST;
-		window.add(binLabel, gbc);
+		GUIPanel.add(binLabel, gbc);
 		gbc.gridx = 0;
 		gbc.anchor = GridBagConstraints.EAST;
-		window.add(binBox, gbc);
+		GUIPanel.add(binBox, gbc);
 		gbc.gridy = 4;
 		gbc.gridx = 1;
 		gbc.anchor = GridBagConstraints.WEST;
-		window.add(visionLabel, gbc);
+		GUIPanel.add(visionLabel, gbc);
 		gbc.gridx = 0;
 		gbc.anchor = GridBagConstraints.EAST;
-		window.add(visionBox, gbc);
+		GUIPanel.add(visionBox, gbc);
 		gbc.gridy = 5;
 		gbc.gridx = 1;
 		gbc.anchor = GridBagConstraints.WEST;
-		window.add(litterLabel, gbc);
+		GUIPanel.add(litterLabel, gbc);
 		gbc.gridx = 0;
 		gbc.anchor = GridBagConstraints.EAST;
-		window.add(litterBox, gbc);
+		GUIPanel.add(litterBox, gbc);
 		gbc.gridx = 2;
 		gbc.gridy = 0;
 		gbc.anchor = GridBagConstraints.CENTER;
-		window.add(indivPointsLabel, gbc);
+		GUIPanel.add(indivPointsLabel, gbc);
 		gbc.gridy = 1;
-		window.add(indivPointsInput, gbc);
+		GUIPanel.add(indivPointsInput, gbc);
 		gbc.gridx = 3;
 		gbc.gridy = 0;
-		window.add(totalPointsLabel, gbc);
+		GUIPanel.add(totalPointsLabel, gbc);
 		gbc.gridy = 1;
-		window.add(totalPointsField, gbc);
+		GUIPanel.add(totalPointsField, gbc);
 		gbc.gridy = 3;
 		gbc.gridx = 3;
 		incrementStackButton.addActionListener(new ActionListener() {
@@ -138,26 +153,26 @@ public class GUI {
 						.parseInt(((JButton) e.getSource()).getText()) + 1));
 			}
 		});
-		window.add(incrementStackButton, gbc);
+		GUIPanel.add(incrementStackButton, gbc);
 		gbc.gridx = 3;
 		gbc.gridy = 5;
-		window.add(stackTimer,gbc);
+		GUIPanel.add(stackTimer,gbc);
 		gbc.gridy = 2;
 		gbc.gridx = 3;
 		stackTimer.setBackground(Color.GREEN);
-		window.add(highestStackLabel, gbc);
+		GUIPanel.add(highestStackLabel, gbc);
 		gbc.gridx = 2;
 		gbc.gridy = 3;
-		window.add(humanPlayerPicker, gbc);
+		GUIPanel.add(humanPlayerPicker, gbc);
 		gbc.gridx = 2;
 		gbc.gridy = 2;
-		window.add(humanPlayerLabel,gbc);
+		GUIPanel.add(humanPlayerLabel,gbc);
 		gbc.gridx = 2;
 		gbc.gridy = 4;
-		window.add(autoTypeLabel, gbc);
+		GUIPanel.add(autoTypeLabel, gbc);
 		gbc.gridx = 2;
 		gbc.gridy = 5;
-		window.add(autoTypePicker, gbc);
+		GUIPanel.add(autoTypePicker, gbc);
 		gbc.gridx = 4;
 		malfunctionButton.addActionListener(new ActionListener() {
 			
@@ -170,15 +185,30 @@ public class GUI {
 			}
 		});
 		malfunctionButton.setBackground(Color.RED);
-		window.add(malfunctionButton, gbc);
+		GUIPanel.add(malfunctionButton, gbc);
 		gbc.gridy = 3;
 		reliabilitySlider.setSnapToTicks(true);
 		reliabilitySlider.setMajorTickSpacing(1);
 		reliabilitySlider.setPaintTicks(true);
 		reliabilitySlider.setPaintLabels(true);
-		window.add(reliabilitySlider, gbc);
+		GUIPanel.add(reliabilitySlider, gbc);
 		gbc.gridy = 2;
-		window.add(reliabilityLabel, gbc);
+		GUIPanel.add(reliabilityLabel, gbc);
+		gbc.gridx = 4;
+		gbc.gridy = 0;
+		GUIPanel.add(matchStart);
+		gbc.gridx = 5;
+		gbc.gridy = 0;
+		GUIPanel.add(matchEnd);
+		
+		// commentPanel stuff
+		commentPanel.setPreferredSize(new Dimension(800,200));
+		commentPanel.add(commentsLabel,BorderLayout.NORTH);
+		commentContainer.setPreferredSize(new Dimension(800,150));
+		commentPanel.add(commentContainer,BorderLayout.SOUTH);
+		// Border Layout for the GUIPanel and commentPanel top bottom respectively
+		window.add(GUIPanel,BorderLayout.PAGE_START);
+		window.add(commentPanel,BorderLayout.PAGE_END);
 		window.pack();
 		window.setLocationRelativeTo(null);
 	}
